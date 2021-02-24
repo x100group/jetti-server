54e6eb352a914ba00eba235c01e74e6e59d7256d
gimpon <dbez@outlook.com>
2021-02-24 22:29:11 +0200

    uset service

--- server/routes/user.settings.ts
+++ server/routes/user.settings.ts
@@ -9,26 +9,6 @@ export const router = express.Router();
 export function User(req: Request): IJWTPayload {
   return (<any>req).user as IJWTPayload;
 }
-
-router.get('/user/settings/:type/:user', async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    // const user = User(req);
-    const sdb = SDB(req);
-    res.json(await UserSettingsManager.getSettings({
-      type: req.params.type || '',
-      user: req.params.user || '',
-      description: req.body.description || '',
-      id: req.body.id || ''
-    }, sdb));
-  } catch (err) { next(err); }
-});
-
-// router.post('/user/settings', async (req, res, next) => {
-//   try {
-//     res.json(await UserSettingsManager.saveSettingsArray(req.body, SDB(req)));
-//   } catch (err) { next(err); }
-// });
-
 router.post('/user/settings', async (req, res, next) => {
   try {
     switch (req.body.command) {
@@ -38,6 +18,14 @@ router.post('/user/settings', async (req, res, next) => {
       case 'delete':
         await UserSettingsManager.deleteSettingsById(req.body.id, SDB(req));
         res.json({});
+        break;
+      case 'get':
+        const { type, user, id } = req.body;
+        res.json(await UserSettingsManager.getSettings({
+          type: type || '',
+          user: user || '',
+          id: id || ''
+        }, SDB(req)));
         break;
       default:
         res.status(405);
